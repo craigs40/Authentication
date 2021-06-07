449e35f34644c8de90c23578358228bb762026c2
Shay Craig <71286979+craigs40@users.noreply.github.com>
2021-06-07 17:38:33 -0400

    Update controller methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,19 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: %i[new create]
 
   def new
     @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to root_path
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
