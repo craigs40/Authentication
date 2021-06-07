31659b1a9134669dba014b60763c8a99c194e921
Shay Craig <71286979+craigs40@users.noreply.github.com>
2021-06-07 18:55:57 -0400

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,4 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-
-  def show
-  end
 end
--- db/migrate/20210607174244_devise_create_posts.rb
+++ db/migrate/20210607174244_devise_create_posts.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 class DeviseCreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email, null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
 
       ## Recoverable
       t.string   :reset_password_token
@@ -32,11 +30,10 @@ class DeviseCreatePosts < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps null: false
     end
 
-    add_index :posts, :email,                unique: true
+    add_index :posts, :email, unique: true
     add_index :posts, :reset_password_token, unique: true
     # add_index :posts, :confirmation_token,   unique: true
     # add_index :posts, :unlock_token,         unique: true
