83bc8ee33f0bc6682c6904ea8d2e42e34e1740df
Shay Craig <71286979+craigs40@users.noreply.github.com>
2021-06-07 17:27:05 -0400

    Update posts controller and form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,16 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+    @post = Post.new
   end
 
   def create
   end
 
   def index
+  end
+
+  def show
   end
 end
