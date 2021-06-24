67239d3b621ba5eccb1d7d9d234588bb6eb045ef
Shay Craig <71286979+craigs40@users.noreply.github.com>
2021-06-24 17:20:53 -0400

    Update routes and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,28 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: %i[new create index]
-
   def new
     @post = Post.new
+    @posts = Post.all
   end
 
   def create
     @post = Post.new(post_params)
     @post.user.id = current_user.id
+    if @post.save
+      redirect_to @post
+    end
   end
 
   def index
     @post = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   get 'posts/index'
   devise_for :posts
   devise_for :users
-  root to: 'posts#new'
-  resources :posts, only: [:new, :create, :index]
+  devise_scope :user do
+    authenticated :user do
+      root 'posts#new', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+  resources :posts, only: %i[new create index show]
 end
